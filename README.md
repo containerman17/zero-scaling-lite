f9dc0a28dc3d02518d16fa34fb681c69123ae8cc
Ilia Home <solohin.i@gmail.com>
2019-11-15 13:51:17 -0500

    putToSleep is ready to do

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Zero scaler for Kubernetes
 Simple, straightforward, lightweight. Works on metrics from Nginx Ingress. Depends on Prometheus to retrieve the metrics. 
 
-
 ## How it works 
 
 ### Proxy component
@@ -9,7 +8,7 @@ On request received, holds it, asks scaler for scaling up to 1 replica.
 
 ### Downscaler
 Every minute:
-Checks ingress with `zero-scaling\sleep_enabled` metrics every 60 seconds. If ingress has traffic after `zero-scaling\sleep_after` seconds, scales down deployment `zero-scaling\deployment_name` to zero and redirect all traffic to proxy.
+Checks ingress with `zero-scaling/enabled = "true"` annotation every 60 seconds. If ingress has traffic after `zero-scaling/sleep-after` seconds, scales down deployment `zero-scaling\deployment_name` to zero and redirect all traffic to proxy. Original proxy value saved in 
 
 ### Upscaler
 HTTP /wakeup/[domainName]
--- downscaler-kubebuider/controllers/checkDownLoop.go
+++ downscaler-kubebuider/controllers/checkDownLoop.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -10,21 +13,75 @@ func checkDownLoop(r *ScalingBackInfoReconciler) {
 	log := r.Log
 	log.V(1).Info("List", "Ingresses on watch", len(ingressesCollection))
 	//	http://prometheus-server.ingress-nginx.svc.cluster.local:9090/api/v1/query?query=
-	prometheusURL := "http://prometheus.test.vscodecloud.com/api/v1/query?query=sum%20by%20(host)%20(%0A%20%20rate(nginx_ingress_controller_bytes_sent_sum%5B3m%5D)%0A)"
+	ingressMap, err := getIngressMap("5m", r)
+
+	log.V(1).Info("Got ingress map", "ingressMap", ingressMap)
+
+	for _, ingress := range ingressesCollection {
+		namespacedName := ingress.Namespace + "/" + ingress.Name
+		mapValue, keyExists := ingressMap[namespacedName]
+		hasTraffic := mapValue && keyExists
+		log.V(1).Info("Got ingress data", "hasTraffic", hasTraffic, "namespacedName", namespacedName)
+
+		if !hasTraffic {
+			putToSleep(ingress.Name, ingress.Namespace, r)
+		}
+	}
+
+	if err != nil {
+		log.Error(err, "Got ingress map error")
+		return
+	}
+}
+
+func putToSleep(ingressName string, ingressNamespace string, r *ScalingBackInfoReconciler) {
+	log := r.Log
+	log.V(1).Info("putToSleep", "ingressName", ingressName, "ingressNamespace", ingressNamespace)
+}
+
+func getIngressMap(timing string, r *ScalingBackInfoReconciler) (map[string]bool, error) {
+	log := r.Log
+	// log.V(1).Info("Get ingress data started")
+
+	query := fmt.Sprintf("sum by (ingress, namespace) ( rate(nginx_ingress_controller_bytes_sent_sum[%s]) )", timing) //TODO unhardcode
+	prometheusURL := "http://prometheus.test.vscodecloud.com/api/v1/query?query=" + url.QueryEscape(query)
 
 	var response Response
 	err := getJson(prometheusURL, &response)
 	if err != nil {
 		log.WithValues("prometheusURL", prometheusURL)
 		log.Error(err, "unable to retrueve information from prometheus")
-		return
+		return map[string]bool{}, err
 	}
 
-	log.V(1).Info("Got response", "response", response)
+	// log.V(1).Debug("Got response", "response", response)
 
+	result := make(map[string]bool)
+
+	for _, resultLine := range response.Data.Result {
+		ingressName, ok := resultLine.Metric["ingress"]
+		if !ok {
+			continue
+		}
+
+		ingressNamespace, ok := resultLine.Metric["namespace"]
+		if !ok {
+			continue
+		}
+
+		byteRate, err := strconv.ParseFloat(resultLine.Value[1].(string), 64)
+		if err != nil {
+			return map[string]bool{}, err
+		}
+		hasTraffic := byteRate > 0
+
+		// log.V(1).Info("Got ingress byterate", "byteRate", byteRate, "ingressName", ingressName, "hasTraffic", hasTraffic)
+
+		result[ingressNamespace+"/"+ingressName] = hasTraffic
+	}
+
+	return result, nil
 }
-
-func get
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
@@ -42,7 +99,7 @@ type Data struct {
 
 type Result struct {
 	Metric map[string]string `json:"metric"`
-	Value  interface{}       `json:"value"`
+	Value  []interface{}     `json:"value"`
 }
 
 func getJson(url string, target interface{}) error {
